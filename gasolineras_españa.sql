8ff652cb54819cbcfbfca773d06bfbe477c13a34
Guillem Escuder Heredia <143967216+gescuderh@users.noreply.github.com>
2024-08-01 20:17:13 +0200

    Add files via upload

---
+++ gasolineras_españa.sql
@@ -0,0 +1,29 @@
+# Creamos la database donde vamos alojar el archivo csv que vamos a importar
+CREATE DATABASE gasolineras_españa;
+
+# Seleccionamos la base de datos creada
+USE gasolineras_españa;
+
+# Mostramos el contenido de la tabla generada al importar el archivo csv
+SELECT * FROM gasolineras_españa;
+
+# Eliminamos las columnas que no vamos a utilizar
+
+
+# ¿Cuál es la provincia con más gasolineras? ¿Y la provincia con menos gasolineras?
+
+# ¿Número de gasolineras por provincia?
+
+# ¿Qué compañía tiene más gasolineras?
+
+# ¿Dónde se encuentra la gasolinera con el precio más alto de gasolina 95? ¿Y la dirección de la gasolinera con el precio más bajo de gasolina 95?
+
+# ¿Cuántos tipos de gcombustible se comercializan al público?
+
+# ¿Cuál es el tipo de combustible que menos se comercializa en las gasolineras?
+
+# ¿Número de gasolineras por tipo de combustible?
+
+# ¿Cuántas gasolineras están abiertas 24h?
+
+# ¿Cuál es el precio medio por tipo de combustible?
